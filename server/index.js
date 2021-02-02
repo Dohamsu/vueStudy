28e86ee13dd20473cd7d9648bd79af42e9ea639e
dohamsu <rlawlsdnjswk@naver.com>
2021-02-02 19:02:04 +0900

    commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => res.send('ㅎㅔ로 월드 '));
+app.get('/', (req,res) => res.send('gegeg Node 월드 '));
 
 app.listen(3000,() => {
-    console.log('무야호  ');
+    console.log('서버가 시작되었습니다.  ');
 })
