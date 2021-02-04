a76492249e624ab38c57e5a795f4836693c1a757
dohamsu <rlawlsdnjswk@naver.com>
2021-02-04 21:06:28 +0900

    commit

--- server/index.js
+++ server/index.js
@@ -17,22 +17,26 @@ const client = new Client({
 // DB 연결
 client.connect();
 
-client.query("SET TIME ZONE 'Asia/Seoul'", (err, res) => {});
-
-client.query("INSERT INTO connect_list (user_id) VALUES ('mcnc')", (err, res) => {
-
-    // console.log("접속이력 생성 완료");
+client.query("SET TIME ZONE 'Asia/Seoul'", (err, res) => {
+    console.log(err);
+    console.log(res);
 });
+client.query("SHOW DATABASES", (err, res) => {});
 
-client.query("SELECT user_id,TO_CHAR(register_date,'YYYY-MM-DD-HH24:MM:SS') as TIME FROM connect_list  order by register_date DESC", (err, res) => {
-    if(err == null){
-        console.log("=========  DB 접속 이력  =======");
-        console.table(res.rows);
-    }else{
-        console.log(err);
-    }
-    client.end() //DB 연결 해제 
-});
+// client.query("INSERT INTO connect_list (user_id) VALUES ('mcnc')", (err, res) => {
+
+//     // console.log("접속이력 생성 완료");
+// });
+
+// client.query("SELECT user_id,TO_CHAR(register_date,'YYYY-MM-DD-HH24:MM:SS') as TIME FROM connect_list  order by register_date DESC", (err, res) => {
+//     if(err == null){
+//         console.log("=========  DB 접속 이력  =======");
+//         console.table(res.rows);
+//     }else{
+//         console.log(err);
+//     }
+//     client.end() //DB 연결 해제 
+// });
 
 
 app.listen(3000,() => {
