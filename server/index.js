e57c596bcb468217ae3d958cceae0c3b207ca0ea
dohamsu <rlawlsdnjswk@naver.com>
2021-02-09 12:25:24 +0900

    Comiit

--- server/index.js
+++ server/index.js
@@ -4,62 +4,102 @@ const { Client } = require('pg');
 
 app.get('/', (req,res) => res.send('gegeg Node 월드 '));
 
+
+
+app.listen(50000,() => {
+
+   
+
 //DB 정보 세팅 
 const client = new Client({
     user : 'mcnc',
-    host : 'localhost',
+    host : 'vue-db',
     database : 'vuedb',
     password : 'gomcnc!!',
-    port : 15432,
+    port : 5432,
 });
-
 
 // DB 연결
 client.connect();
 
-client.query("SHOW DATABASES", (err, res) => {
-
-    // console.log(res);
-
-});
-
-client.query("CREATE TABLE ACCOUNT"
- +"("
- + "USER_ID SERIAL PRIMARY KEY,"
- + "USERNAME VARCHAR(50) UNIQUE NOT NULL,"
- + "PASSWORD VARCHAR(50) NOT NULL,"
- + "EMAIL VARCHAR(355) UNIQUE NOT NULL,"
- + "CREATED_ON TIMESTAMP NOT NULL,"
- + "LAST_LOGIN TIMESTAMP"
- + ")", (err, res) => {
-    console.log(res);
-});
-
-
-client.query("CREATE TABLE connect_list"
- +"("
- + "USER_ID VARCHAR(50) NOT NULL,"
- + "register_date NOW()"
- + ")", (err, res) => {
-
-    console.log(res);
-
-});
-
-client.query("INSERT INTO connect_list (user_id) VALUES ('mcnc')", (err, res) => {
-
-    // console.log("접속이력 생성 완료");
-});
-
-client.query("SELECT user_id,TO_CHAR(register_date,'YYYY-MM-DD-HH24:MM:SS') as TIME FROM connect_list  order by register_date DESC", (err, res) => {
+client.query("SELECT * FROM pg_tables WHERE tablename = 'account'", (err, res) => {
+    //계정 테이블 없을 경우 생성
     if(err == null){
-        console.log("=========  DB 접속 이력  =======");
-        console.table(res.rows);
+        if(res.rows.length<1){
+            client.query("CREATE TABLE ACCOUNT"
+            +"("
+            + "USER_ID SERIAL PRIMARY KEY,"
+            + "USERNAME VARCHAR(50) UNIQUE NOT NULL,"
+            + "PASSWORD VARCHAR(50) NOT NULL,"
+            + "EMAIL VARCHAR(355) UNIQUE NOT NULL,"
+            + "CREATED_ON TIMESTAMP NOT NULL,"
+            + "LAST_LOGIN TIMESTAMP"
+            + ")", (err, res) => {
+            });
+        }
+    }
+});
+
+client.query("SELECT * FROM pg_tables WHERE tablename = 'connect_list'", (err, res) => {
+    if(err == null){
+        //접속 테이블 없을 경우 생성
+        if(res.rows.length<1){
+            createTable();
+        } 
+        //접속 이력 테이블이 있을 경우 조회 
+        else{
+            insertCurrentTime();
+            lookList();
+        }
     }else{
+        console.log("에러러ㅓ어어어어어러러어어어");
         console.log(err);
     }
-    client.end() //DB 연결 해제 
 });
+
+function lookList(){
+    client.query("SELECT user_id,TO_CHAR(register_date,'YYYY-MM-DD-HH24:MM:SS') as TIME FROM connect_list  order by register_date DESC", (err, res) => {
+        if(err == null){
+            console.log("=========  DB 접속 이력  =======");
+            console.table(res.rows);
+            endDBconnection();
+        }else{
+            console.log(err);
+        }
+    });
+    
+    //endDBconnection(); //db 접속 종료
+}
+
+function createTable() {
+    client.query("CREATE TABLE connect_list"
+    +"("
+    + "USER_ID VARCHAR(50) NOT NULL,"
+    + "register_date timestamp"
+    + ")", (err, res) => {
+        console.log("접속 이력 테이블 생성 완료");
+    });  
+}
+
+function insertCurrentTime() {
+    client.query("INSERT INTO connect_list (user_id,register_date) VALUES ('mcnc',now())", (err, res) => {
+
+
+        if(err == null){
+            console.log("접속이력 생성 완료 \n\n");
+
+        }else{
+            console.log(err);
+        }
+    });
+}
+
+function endDBconnection(){
+    client.end() //DB 연결 해제 
+}
+
+
+})
 
 
 app.listen(3000,() => {
