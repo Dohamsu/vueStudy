bdcdc8438db538dd68968b9d595d6d8f200a6e49
dohamsu <rlawlsdnjswk@naver.com>
2021-02-09 18:34:33 +0900

    comiit

--- client/main/js/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req,res) => res.send('app,js로 월드 '));
-
-app.listen(3000,() => {
-    console.log('무야호  app.js');
-})
--- client/main/js/test.js
+++
@@ -1,7 +0,0 @@
-'use strinct';
-
-var printFunc = function(name){
-    console.log(name);
-}
-
-printFunc('vs cononoo');
