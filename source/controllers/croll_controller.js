64e89c84d0d10f21d571cc62b435eb305a6d9461
dohamsu <rlawlsdnjswk@naver.com>
2021-03-03 14:41:05 +0900

    commit

--- source/controllers/croll_controller.js
+++ source/controllers/croll_controller.js
@@ -7,7 +7,9 @@ exports.getHtml = async(req, res, next) => {
     try {
         console.log("크롤 컨트롤러");
         let resultData = await CrollService.getHtml(url);
-        return res;
+        console.log("data : ");
+        console.log(resultData);
+        return res.status(200).json(resultData);
     }catch(err){
         return res.status(500).json(err);
     }
--- source/services/croll_service.js
+++ source/services/croll_service.js
@@ -1,61 +1,60 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const iconv = require("iconv-lite");
 
-exports.getHtml = async () => {
+exports.getHtml = () => {
     try {
 
-        console.log("크롤링중");
-      
-        let html = await axios.get("https://www.yna.co.kr/sports/all");
-      
-        let ulList = [];
-        const $ = cheerio.load(html.data);
-        const $bodyList = $("div.headline-list ul").children("li.section02");
-    
-        $bodyList.each(function(i, elem) {
-            ulList[i] = {
-                title: $(this).find('strong.news-tl a').text(),
-                url: $(this).find('strong.news-tl a').attr('href'),
-                image_url: $(this).find('p.poto a img').attr('src'),
-                image_alt: $(this).find('p.poto a img').attr('alt'),
-                summary: $(this).find('p.lead').text().slice(0, -11),
-                date: $(this).find('span.p-time').text()
-            };
-        });
-    
-        const data = ulList.filter(n => n.title);
+        var result = "dldl";
 
-        console.log(data);
+        console.log("크롤링중....\n");
+      
+        const getContent = async () => {
+            try{
 
-        return data;
+                const html = await axios({
+                    url: "https://naver.com",
+                    mehthod :"GET",
+                    responseType : "arraybuffer"
+                });
+            //    console.log(html);
+                const contents = iconv.decode(html.data,"UTF-8");
+                // console.log(contents);
+               return contents; 
+            }
+            catch(error){
+                console.error(error);
+            }
+        }
+        
+        const crollResult = async () => {
+            return getContent()
+                .then(html =>{
+                    // console.log(html.data);
+                    let ulList = [];
+                    const $ = cheerio.load(html);
+                    const $bodyList = $("ul.list_theme");
+                
+                    console.log("===========  첫번째 돔 ======================");
+                    $bodyList.each(function(i, elem) {
+                        ulList[i] = {
+                            title: $(this).find('em.theme_category').first().text(),
+                            summary: $(this).find('strong.title.elss').text()
+                        };
+                    });
+                    // console.log(ulList);
+                    const data = ulList.filter(n => n.title);
+                console.log("==============마지막 필터링 데이터 =================");
+                console.log(JSON.stringify(data));
+                
+                return data;
+                })
+        }
+        
+        return  crollResult();
+
     } catch (error) {
       console.error(error);
     }
-
-
-
-getHtml()
-.then(html => {
-    let ulList = [];
-    const $ = cheerio.load(html.data);
-    const $bodyList = $("div.headline-list ul").children("li.section02");
-
-    $bodyList.each(function(i, elem) {
-    ulList[i] = {
-        title: $(this).find('strong.news-tl a').text(),
-        url: $(this).find('strong.news-tl a').attr('href'),
-        image_url: $(this).find('p.poto a img').attr('src'),
-        image_alt: $(this).find('p.poto a img').attr('alt'),
-        summary: $(this).find('p.lead').text().slice(0, -11),
-        date: $(this).find('span.p-time').text()
-    };
-    });
-
-    const data = ulList.filter(n => n.title);
-    return data;
-})
-.then(res => log(res));
-                                                                                 
-
 };
   
